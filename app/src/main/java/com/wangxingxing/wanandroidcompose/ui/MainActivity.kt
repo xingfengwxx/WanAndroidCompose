9a324b52431364988b599a94c5eb9937909e7450
WangXingxing <1099420259@qq.com>
2025-04-17 15:49:31 +0800

    返回键处理

--- app/src/main/java/com/wangxingxing/wanandroidcompose/ui/MainActivity.kt
+++ app/src/main/java/com/wangxingxing/wanandroidcompose/ui/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.wangxingxing.wanandroidcompose.ui
 
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.btpj.lib_base.base.BaseActivity
+import com.btpj.lib_base.utils.ToastUtil
+import com.wangxingxing.wanandroidcompose.R
 import com.wangxingxing.wanandroidcompose.core.navigation.AppScreen
 import com.wangxingxing.wanandroidcompose.core.navigation.LocalNavController
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,6 +25,9 @@ class MainActivity : BaseActivity() {
 
     private lateinit var navHostController: NavHostController
 
+    /** 上一次点击返回键的时间 */
+    private var lastBackMills: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -30,6 +36,24 @@ class MainActivity : BaseActivity() {
             navHostController = rememberNavController()
             CompositionLocalProvider(LocalNavController provides navHostController) {
                 AppScreen()
+            }
+        }
+
+        // 返回处理
+        onBackPressedDispatcher.addCallback(this, mBackPressedCallback)
+    }
+
+    /** 返回监听回调 */
+    private val mBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if (System.currentTimeMillis() - lastBackMills > 2000) {
+                lastBackMills = System.currentTimeMillis()
+                ToastUtil.showShort(
+                    this@MainActivity,
+                    getString(R.string.toast_double_back_exit)
+                )
+            } else {
+                finish()
             }
         }
     }
